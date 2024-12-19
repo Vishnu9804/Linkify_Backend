2482d64ae32dde8a45715ce714cb05dbd2e7213f
Vishnu9804 <burkhawalavishnu@gmail.com>
2024-12-19 17:15:36 +0530

    Database Connection

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const port1 = 3001;
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+require("./db");
+require("./models/User");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server is running on post " + port);
+});
